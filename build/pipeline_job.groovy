a2ea9f95b7630db1fa7b0521150ac10021f75bf4
Abel Terefe <abel.terefe@trademarknow.com>
2019-07-16 11:37:25 +0300

    Add groovy build pipeline

---
+++ build/pipeline_job.groovy
@@ -0,0 +1,16 @@
+pipelineJob('http4s-jenkins') {
+  description('Build scala project')
+  triggers {
+    scm('*/5 * * * *')
+  }
+  definition {
+    cpsScm {
+      scm {
+        git('git@github.com:abelbryo/hello-http4s-jenkins.git') {
+          node -> node / extensions()
+        }
+      }
+      scriptPath('hello-http4s-jenkins/Jenkinsfile')
+    }
+  }
+}
