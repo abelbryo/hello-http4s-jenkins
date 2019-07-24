57365af322c2795a3240dd32eeb174f0f262bc78
Abel Terefe <abel.terefe@trademarknow.com>
2019-07-24 17:10:55 +0300

    Fix script

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ sed -i.bak -e "s|$REPO:.*|$IMAGE|g" -- ./k8s/deployment.yml;
 
 cat k8s/deployment.yml;
 
-# kubectl apply -f k8s/namespace.yml
-# kubectl apply -f k8s/deployment.yml
-# kubectl apply -f k8s/service.yml
+kubectl apply -f k8s/namespace.yml
+kubectl apply -f k8s/deployment.yml
+kubectl apply -f k8s/service.yml
