3b92481b6aca090cf25c9e6f494b7c947bd4154e
Abel Terefe <abel.terefe@trademarknow.com>
2019-07-24 16:12:07 +0300

    Fix script

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 
 REPO='aterefe/ordering-system'
 IMAGE=$1
-if [ -z "$TAG" ]; then;
-  IMAGE="latest"
+if [ -z "$TAG" ]; then
+  IMAGE="$REPO:latest"
 fi
 
-sed -i -e 's/\$REPO:.*/\$IMAGE' k9s/deployment.yml
+sed -i -e 's/\$REPO:.*/\$IMAGE' k9s/deployment.yml;
 
 kubectl apply -f k8s/namespace.yml
 kubectl apply -f k8s/deployment.yml
