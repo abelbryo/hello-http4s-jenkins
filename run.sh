1b6bd84f1d878378d9f14c68e5f4c4eefff6856f
Abel Terefe <abel.terefe@trademarknow.com>
2019-07-24 16:14:12 +0300

    Fix regex

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ if [ -z "$TAG" ]; then
   IMAGE="$REPO:latest"
 fi
 
-sed -i -e 's/\$REPO:.*/\$IMAGE' k9s/deployment.yml;
+sed -i -e 's/\$REPO:.*/\$IMAGE/g' k9s/deployment.yml;
 
 kubectl apply -f k8s/namespace.yml
 kubectl apply -f k8s/deployment.yml
